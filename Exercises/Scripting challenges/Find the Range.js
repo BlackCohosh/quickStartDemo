bcfc9809a219e9a3bf9046fd92786ae269cca3ac
Agriscapes <62765843+Agriscapes@users.noreply.github.com>
2021-10-14 09:33:21 -0500

    Scripting exercises from Bright Paths Program

---
+++ Exercises/Scripting challenges/Find the Range.js
@@ -0,0 +1,19 @@
+var arr = [69,1, 4, 11, 2, 37, -4];
+    arr.sort(function(a, b){return a - b});
+
+var min = arr[0]
+var max = arr[arr.length -1]
+    
+console.log(min, " ", max)
+
+
+//let arr = [1,4,11,2,37,-4];
+//let min = arr[0];
+//let max = arr[0];
+
+//for(let i = 0; < arr.lenth; i++){
+    //if(arr[i] < min){
+        //min = arr[i];
+    //}
+//}
+//console.log("Max is: ", max, " and min is ", ".")
